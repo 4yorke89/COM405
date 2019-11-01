579c9755dedac525683ffe60dba24c7ec0128913
Ethan York <4yorke89@solent.ac.uk>
2019-11-01 15:55:39 +0000

    Lesson end

--- Week6/src/week6/CarPark.java
+++ Week6/src/week6/CarPark.java
@@ -10,41 +10,41 @@ package week6;
  * @author 4yorke89
  */
 public class CarPark {
-   private Car[] spaces = new Car[15];
-   
-   public boolean addCar(Car carIn){
-       for(int count = 0; count < spaces.length; count++){
-           if(spaces[count] == null){
-               spaces[count] = carIn;
-               return true;
-           }
-       }
-       return false;
-   }
-    public boolean removeCar(String noPlateIn){
-       for(int count = 0; count < spaces.length; count++){
-           if(spaces[count] != null){
-               String noPlate = spaces[count].getNoPlate();
-               if (noPlate.equals(noPlateIn)){
-                   spaces[count] = null;
-                   return true;
-               }
-           }
-       }
-       return false;
-   }
-    public Car getCar(String noPlateIn)
-    {
-        for( int count = 0; count < spaces.length; count++){
-            if(spaces[count] != null)
-            {
-                String id = spaces[count].getNoPlate();
-                 if (id.equals(noPlateIn)){
-                     return spaces[count];
-                 }
-                
+
+    private Car[] spaces = new Car[15];
+
+    public boolean addCar(Car carIn) {
+        for (int count = 0; count < spaces.length; count++) {
+            if (spaces[count] == null) {
+                spaces[count] = carIn;
+                return true;
             }
-            return null;
         }
-}
+        return false;
+    }
+
+    public boolean removeCar(String noPlateIn) {
+        for (int count = 0; count < spaces.length; count++) {
+            if (spaces[count] != null) {
+                if (spaces[count].getNoPlate().equals(noPlateIn)) {
+                    spaces[count] = null;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Car getCar(String noPlateIn) {
+        for (int count = 0; count < spaces.length; count++) {
+            if (spaces[count] != null) {
+                if (spaces[count].getNoPlate().equals(noPlateIn)) {
+                    return spaces[count];
+                }
+
+            }
+
+        }
+        return null;
+    }
 }
--- Week6/src/week6/Week6.java
+++ Week6/src/week6/Week6.java
@@ -5,6 +5,8 @@
  */
 package week6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 4yorke89
@@ -15,7 +17,32 @@ public class Week6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CarPark cp = new CarPark();
+        
+        Scanner scanIn = new Scanner(System.in);
+        
+        int state = 1;
+        
+        while(state == 1)
+                {
+                    System.out.println("A - Add Car");
+                    System.out.println("B - remove Car");
+                            System.out.println("X - Exit");
+                            String input = scanIn.nextLine();
+                            if(input.equals("A"))
+                            {
+                                System.out.println("Please enter Number Plate");
+                            }
+                            else(input.equals("B"))
+                                    {
+                                        
+                                    }
+                            
+                               else(input.equals("X")) 
+                                       {
+                                           state = 0;
+                                       }
+                }
     }
-    
+
 }
