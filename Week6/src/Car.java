5028d1463c95a7cc8799e767343a73010092372d
Ethan York <4yorke89@solent.ac.uk>
2019-11-01 13:44:01 +0000

    car class

---
+++ Week6/src/Car.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 4yorke89
+ */
+public class Car {
+  private String noPlate;
+  
+  public Car(String noPlatein){
+      noPlate = noPlatein;
+  }
+  public String getNoPlate(){
+      return noPlate;
+  }
+}
